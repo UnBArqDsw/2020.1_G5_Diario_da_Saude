2c2d54ae8e09b6e928e888877ffacef506640b11
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2020-10-20 18:57:54 -0300

    Feature/Cruando rotas de ubs

--- backend/DiarioSaude/models/ubs.model.js
+++ backend/DiarioSaude/models/ubs.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const UbsSchema = mongoose.Schema({
     ubsName: {type: String, required: true},
     city : {type: String, required: true},
-    state : {type: String, required: true}    
+    state : {type: String, required: true},
+    cep : {type: Number, required: true}
 }, {
     timestamps: true
 });
